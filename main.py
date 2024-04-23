ca2f7fc8c44e0d05eb04987b5e555bb86f813613
kaanaygen8 <gun_aygen@brown.edu>
2024-04-23 18:27:16 -0400

    alem2006

--- main.py
+++ main.py
@@ -99,10 +99,9 @@ class runModel:
         data = data_preprocessor.get_preprocessed_data()
 
         X_train, X_test, y_train, y_test = self.split_normalize_XY(data)
-        tensor_X_train = torch.tensor(X_train, dtype=torch.float).unsqueeze(1)
-        tensor_y_train = torch.tensor(y_train, dtype=torch.float)
-        tensor_X_test = torch.tensor(X_test, dtype=torch.float).unsqueeze(1)
-        tensor_y_test = torch.tensor(y_test, dtype=torch.float)
+        tensor_X_train = torch.tensor(X_train, dtype=torch.float32)
+        tensor_y_train = torch.tensor(y_train, dtype=torch.float32)
+        tensor_y_test = torch.tensor(y_test, dtype=torch.float32)
 
         train_dataset = TensorDataset(tensor_X_train, tensor_y_train)
         test_dataset = TensorDataset(tensor_X_test, tensor_y_test)
