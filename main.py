29f41070e6feef9ead18886c138f62a3bc162915
kaanaygen8 <gun_aygen@brown.edu>
2024-04-23 18:29:15 -0400

    alem2006

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ class runModel:
 
         X_train, X_test, y_train, y_test = self.split_normalize_XY(data)
         tensor_X_train = torch.tensor(X_train, dtype=torch.float32)
+        tensor_X_test = torch.tensor(X_test, dtype=torch.float32)
         tensor_y_train = torch.tensor(y_train, dtype=torch.float32)
         tensor_y_test = torch.tensor(y_test, dtype=torch.float32)
 
