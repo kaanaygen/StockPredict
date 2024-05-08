273d70598f41b4acea78a2e975fceb264d9e41a9
kaanaygen8 <gun_aygen@brown.edu>
2024-05-08 14:38:32 -0400

    11

--- CNN.py
+++ CNN.py
@@ -12,9 +12,9 @@ class CNN(nn.Module):
     def __init__(self, num_tickers, num_features):
 
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings=num_tickers, embedding_dim=20)
+        self.embedding = nn.Embedding(num_embeddings=num_tickers, embedding_dim=33)
 
-        self.conv1 = nn.Conv1d(in_channels= 20 + num_features, out_channels=16, kernel_size=3, stride = 1)
+        self.conv1 = nn.Conv1d(in_channels= 33 + num_features, out_channels=16, kernel_size=3, stride = 1)
         self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride = 1)
         self.conv3 = nn.Conv1d(in_channels=32, out_channels= 64, kernel_size=3, stride = 1)
         self.flatten = nn.Flatten()
