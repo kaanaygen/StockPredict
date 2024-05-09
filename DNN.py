c08b2601ea2b747ba5ffe89fac219c380f127696
kaanaygen8 <gun_aygen@brown.edu>
2024-05-08 22:21:43 -0400

    11

--- DNN.py
+++ DNN.py
@@ -12,7 +12,7 @@ class DNN(nn.Module):
         ticker_embedding_dim = num_tickers
         concat_input_size = ticker_embedding_dim + num_features
         self.hidden_layers_size = [4056, 2048, 1024, 512, 256, 128, 64, 32, 16, 1]
-        self.ticker_embedding = nn.Embedding(num_embeddings=num_tickers + 1, embedding_dim=ticker_embedding_dim)
+        self.ticker_embedding = nn.Embedding(num_embeddings=num_tickers, embedding_dim=8)
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(concat_input_size, self.hidden_layers_size[0]))
         self.layers.append(nn.ReLU())
